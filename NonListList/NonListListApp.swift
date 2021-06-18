27239aaa97431186417ca77acb0df852a9d509ce
Eric Bright <ericbright2002@yahoo.com>
2021-06-17 20:08:48 -0400

    Initial Commit

---
+++ NonListList/NonListListApp.swift
@@ -0,0 +1,17 @@
+//
+//  NonListListApp.swift
+//  NonListList
+//
+//  Created by Eric Bright on 6/17/21.
+//
+
+import SwiftUI
+
+@main
+struct NonListListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
